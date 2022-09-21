8f332cc639ff7c3cde7ad336ffe0f5fca8389c21
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-21 15:18:23 -0300

    tasks 1-3 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar.</p>);
+    return (
+      <div>
+        <Header />
+        <SolarSystem />
+        <Title />
+      </div>
+    );
   }
 }
 
